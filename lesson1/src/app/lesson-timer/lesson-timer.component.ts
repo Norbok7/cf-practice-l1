f210fe65c012d92b05417c14a2c96af46412faf6
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2024-08-31 01:05:34 -0500

    BUG FIX for better timer

--- lesson1/src/app/lesson-timer/lesson-timer.component.ts
+++ lesson1/src/app/lesson-timer/lesson-timer.component.ts
@@ -15,15 +15,19 @@ export class LessonTimerComponent implements OnInit, OnDestroy {
   timeRemaining: number = this.timeLimit;
   progressWidth: number = 100; // Initialize progress width to full
   timerSubscription!: Subscription;
+  startTime: number = 0;
+  animationFrameId!: number;
 
   ngOnInit() {
     this.startTimer();
   }
 
   startTimer() {
+    this.startTime = Date.now();
     this.timerSubscription = interval(1000).subscribe(() => {
       if (this.timeRemaining > 0) {
         this.timeRemaining--;
+        this.updateProgress();
       } else {
         this.timerSubscription.unsubscribe();
         this.timerExpired.emit(); // Notify parent component that timer expired
@@ -31,8 +35,17 @@ export class LessonTimerComponent implements OnInit, OnDestroy {
     });
   }
 
+  updateProgress() {
+    cancelAnimationFrame(this.animationFrameId);
+    const duration = (Date.now() - this.startTime) / 1000;
+    this.progressWidth = Math.max(0, ((this.timeLimit - duration) / this.timeLimit) * 100);
+    this.animationFrameId = requestAnimationFrame(() => this.updateProgress());
+  }
+
   resetTimer() {
     this.timeRemaining = this.timeLimit;
+    this.startTime = Date.now();
+    this.updateProgress(); // Reset progress bar
     if (this.timerSubscription) {
       this.timerSubscription.unsubscribe();
     }
@@ -43,28 +56,16 @@ export class LessonTimerComponent implements OnInit, OnDestroy {
     if (this.timerSubscription) {
       this.timerSubscription.unsubscribe();
     }
+    cancelAnimationFrame(this.animationFrameId);
   }
 
   getProgressBarBackground(): string {
     return this.timeRemaining <= 10
-      ? 'linear-gradient(115deg, rgba(255, 0, 0, 0.25), rgba(255, 94, 94, 0.25) 73%, rgba(255, 0, 0, 0.25))' // Updated red gradient with 25% opacity
-      : 'linear-gradient(115deg, #166534, #15803d 73%, #14532d)'; // Original gradient
+      ? 'linear-gradient(115deg, rgba(255, 0, 0, 0.25), rgba(255, 94, 94, 0.25) 73%, rgba(255, 0, 0, 0.25))'
+      : 'linear-gradient(115deg, #166534, #15803d 73%, #14532d)';
   }
 
-  updateProgress() {
-    this.progressWidth = (this.timeRemaining / this.timeLimit) * 100;
-  }
-
-  // Call this method when a new question is presented
   handleNewQuestion() {
-    this.animateProgressBar();
     this.resetTimer();
-  }
-
-  private animateProgressBar() {
-    this.progressWidth = 0; // Start with empty progress
-    setTimeout(() => {
-      this.progressWidth = 100; // Fill the progress bar
-    }, 0); // Start animation immediately
   }
 }
